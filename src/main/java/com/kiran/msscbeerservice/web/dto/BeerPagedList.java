52bd9117688579bbf35fb2f288f31f3c1d338816
Kiran_B <bhatkiran74@gmail.com>
2023-07-05 00:37:37 +0530

    add beer controller #3

--- src/main/java/com/kiran/msscbeerservice/web/dto/BeerPagedList.java
+++ src/main/java/com/kiran/msscbeerservice/web/dto/BeerPagedList.java
@@ -9,7 +9,6 @@ public class BeerPagedList extends PageImpl<BeerDto> {
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
-
     public BeerPagedList(List<BeerDto> content) {
         super(content);
     }
